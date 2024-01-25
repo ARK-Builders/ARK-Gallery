859cbd05677d321c4445c0c4694e6b993f7cf6db
prokawsar <prokawsar@gmail.com>
2024-01-25 10:18:54 +0600

    button

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,13 @@
-<h1 class="text-3xl font-bold underline">
-  Hello world!
-</h1>
+<script lang="ts">
+	import Fa from 'svelte-fa'
+	import {faImage} from '@fortawesome/free-regular-svg-icons'
+</script>
+<div class="flex flex-col max-w-7xl p-5 w-full rounded-md">
+	<div class="flex flex-row gap-5 px-5 justify-end mt-10">
+		<button class="flex flex-row items-center text-white gap-2 rounded-lg bg-blue-400 hover:bg-blue-600 px-4 py-2">
+			<Fa icon={faImage} />
+			<span>Select images</span>
+		</button>
+	</div>
+
+</div>
