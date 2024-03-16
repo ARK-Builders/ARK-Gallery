265b60ab8086327d9fe80213c625f11c53c8b5c1
alvinosh <38663469+alvinosh@users.noreply.github.com>
2024-03-16 11:10:30 +0100

    #5:  Integrate the app with Tauri framework (#6)
    * Tauri integration
    * GitHub Actions workflow files
    ---------
    Co-authored-by: Kirill Taran <kirill@taran.space>

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-vercel'
+import adapter from '@sveltejs/adapter-static' // This was changed from adapter-auto
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 
 /** @type {import('@sveltejs/kit').Config} */
