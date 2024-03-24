c538fd2b94c7a9b37b64a8272a03198432d45d2e
Kawsar Ahmed <prokawsar@gmail.com>
2024-03-24 11:21:17 +0600

    Added toaster

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,7 @@
 	import Modal from '$lib/components/Modal.svelte'
 	import { galleryStore } from '$lib/store'
 	import '../app.css'
+	import { Toaster } from 'svelte-sonner'
 </script>
 
 <main class="flex bg-gray-100">
@@ -9,4 +10,5 @@
 	{#if $galleryStore.modal}
 		<Modal />
 	{/if}
+	<Toaster />
 </main>
