e74d007e49b7f911aa036faeae871e049e84ee84
Kawsar Ahmed <prokawsar@gmail.com>
2024-03-28 11:46:57 +0600

    Viewer.js Implemented

--- src/lib/components/Gallery.svelte
+++ src/lib/components/Gallery.svelte
@@ -3,12 +3,38 @@
 	import Image from '$lib/components/Image.svelte'
 	import Fa from 'svelte-fa'
 	import { faBan } from '@fortawesome/free-solid-svg-icons'
+	import 'viewerjs/dist/viewer.css'
+	import Viewer from 'viewerjs'
+	import { onMount } from 'svelte'
 
+	let imgContainerRef: HTMLElement | null = null
+	let viewer
 	$: noImage = $galleryStore.selectedTag && !$galleryStore.selectedFilteredImages.length
+
+	onMount(() => {
+		if (imgContainerRef) {
+			viewer = new Viewer(imgContainerRef, {
+				inline: false,
+				toolbar: {
+					zoomIn: 1,
+					zoomOut: 1,
+					reset: 1,
+					prev: 1,
+					play: 0,
+					next: 1,
+					rotateLeft: 1,
+					rotateRight: 1,
+					flipHorizontal: 1,
+					flipVertical: 1
+				}
+			})
+		}
+	})
 </script>
 
 {#if $galleryStore.images?.length}
 	<div
+		bind:this={imgContainerRef}
 		class="gap-3 max-h-[60vh] overflow-auto
 		{noImage
 			? 'flex flex-col gap-5 items-center w-full justify-center'
