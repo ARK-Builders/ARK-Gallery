587971463b60de1d57942e1c22887581e771604c
Kawsar Ahmed <prokawsar@gmail.com>
2024-04-02 11:10:31 +0600

    left side panel

---
+++ src/lib/components/gallery/ActionButton.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import type { IconDefinition } from '@fortawesome/free-regular-svg-icons'
+	import Fa from 'svelte-fa'
+
+	type Method = () => void
+
+	export let text: string = ''
+	export let icon: IconDefinition | null = null
+	export let onClick: Method | null = null
+</script>
+
+<button class="flex flex-col items-center gap-3" on:click={() => (onClick ? onClick() : '')}>
+	{#if icon}
+		<Fa {icon} class="text-gray-700" />
+	{/if}
+
+	<span class="text-sm font-light">{text}</span>
+</button>
--- src/lib/components/gallery/ImageEditor.svelte
+++ src/lib/components/gallery/ImageEditor.svelte
@@ -1,22 +1,28 @@
 <script lang="ts">
 	import { galleryStore } from '$lib/store'
-	import { faRotateLeft, faRotateRight } from '@fortawesome/free-solid-svg-icons'
-	import Fa from 'svelte-fa'
+	import {
+		faCropSimple,
+		faDroplet,
+		faPaintBrush,
+		faRotateLeft,
+		faRotateRight,
+		faT
+	} from '@fortawesome/free-solid-svg-icons'
+
 	import ImageDetails from '$lib/components/gallery/ImageDetails.svelte'
+	import ActionButton from '$lib/components/gallery/ActionButton.svelte'
 
 	export let showInfo = false
 </script>
 
 <div class="mx-auto flex h-[75vh] w-full max-w-7xl flex-row gap-6 py-12">
-	<div class="flex w-36 flex-col gap-3 rounded-xl bg-white py-5 shadow-lg">
-		<button class="flex flex-col items-center gap-1">
-			<Fa icon={faRotateLeft} />
-			<span class="text-sm">Rotate Left</span>
-		</button>
-		<button class="flex flex-col items-center gap-1">
-			<Fa icon={faRotateRight} />
-			<span class="text-sm">Rotate Right</span>
-		</button>
+	<div class="flex w-36 flex-col gap-6 rounded-xl bg-white py-5 shadow-lg">
+		<ActionButton text="Rotate Left" icon={faRotateLeft} />
+		<ActionButton text="Rotate Right" icon={faRotateRight} />
+		<ActionButton text="Brush" icon={faPaintBrush} />
+		<ActionButton text="Blur" icon={faDroplet} />
+		<ActionButton text="Crop" icon={faCropSimple} />
+		<ActionButton text="Text" icon={faT} />
 	</div>
 
 	<div class="h-full max-h-[80vh] w-full">
