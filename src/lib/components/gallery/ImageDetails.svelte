1c33aef02f6f2b8d6f4c133f0a3f52bd46283af7
Kawsar Ahmed <prokawsar@gmail.com>
2024-04-02 11:18:21 +0600

    Detail tab

--- src/lib/components/gallery/ImageDetails.svelte
+++ src/lib/components/gallery/ImageDetails.svelte
@@ -2,44 +2,12 @@
 	import { galleryStore } from '$lib/store'
 	import { faCalendarAlt, faFileAlt } from '@fortawesome/free-regular-svg-icons'
 	import { faListNumeric, faTag } from '@fortawesome/free-solid-svg-icons'
-	import Fa from 'svelte-fa'
+	import DetailField from '$lib/components/gallery/DetailField.svelte'
 </script>
 
-<div class="flex w-56 flex-col gap-5 rounded-xl bg-white px-6 py-5 shadow-lg">
-	<div class="flex w-full flex-col gap-3 border-b-2 border-gray-500 pb-1">
-		<p class="flex items-center gap-3">
-			<Fa icon={faCalendarAlt} />
-			Date
-		</p>
-		<p class="font-bold">
-			{$galleryStore.selectedImage?.lastModified}
-		</p>
-	</div>
-	<div class="flex w-full flex-col gap-3 border-b-2 border-gray-500 pb-1">
-		<p class="flex items-center gap-3">
-			<Fa icon={faTag} />
-			Tag
-		</p>
-		<p class="font-bold">
-			{$galleryStore.selectedImage?.tag}
-		</p>
-	</div>
-	<div class="flex w-full flex-col gap-3 border-b-2 border-gray-500 pb-1">
-		<p class="flex items-center gap-3">
-			<Fa icon={faListNumeric} />
-			Size
-		</p>
-		<p class="font-bold">
-			{$galleryStore.selectedImage?.size}
-		</p>
-	</div>
-	<div class="flex w-full flex-col gap-3 border-b-2 border-gray-500 pb-1">
-		<p class="flex items-center gap-3">
-			<Fa icon={faFileAlt} />
-			Format
-		</p>
-		<p class="font-bold">
-			{$galleryStore.selectedImage?.type}
-		</p>
-	</div>
+<div class="flex w-56 flex-col gap-7 rounded-xl bg-white px-6 py-5 shadow-lg">
+	<DetailField icon={faCalendarAlt} text="Date" value={$galleryStore.selectedImage?.lastModified} />
+	<DetailField icon={faTag} text="Tag" value={$galleryStore.selectedImage?.tag} />
+	<DetailField icon={faListNumeric} text="Size" value={$galleryStore.selectedImage?.size} />
+	<DetailField icon={faFileAlt} text="Format" value={$galleryStore.selectedImage?.type} />
 </div>
